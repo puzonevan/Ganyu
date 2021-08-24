9e90d38bb46b15d01e5995add338bfdc0a129436
‘Evan <puzonevan@gmail.com>
2021-08-24 12:31:04 -0700

    redid layout again...

--- scripts/combatInfo.js
+++ scripts/combatInfo.js
@@ -1,41 +1,41 @@
 const combatInfo = [
     {
         name: "Liuthian Archery", 
-        description: "<strong>Normal Attack:</strong> Perform up to 6 consecutive shots with a bow<br>\
-                    <strong>Charged Attack:</strong>Perform a more precise Aimed Shotwith increased DMG. \
+        description: "Normal Attack: Perform up to 6 consecutive shots with a bow<br><br>\
+                    Charged Attack: Perform a more precise Aimed Shotwith increased DMG. \
                     While aiming, an icy aura will accumulate on the arrowhead before the arrow is fired. \
                     Has different effects based on how long the energy has been charged:<br>\
-                    <strong>Charge Level 1:</strong>Fires off an icy arrow that deals Cryo DMG<br>\
-                    <strong>Charge Level 2:</strong>Fires off a Frostflake Arrow that deals Cryo DMG\
-                    The Frostflake Arrow blooms after hitting its target, dealing AoE Cryo DMG<br>\
-                    <strong>Plunging Attack</strong>Fires off a shower of arrows in mid-air before falling \
+                    &emsp; &emsp; Charge Level 1: Fires off an icy arrow that deals <strong>Cryo DMG</strong><br>\
+                    &emsp; &emsp; Charge Level 2: Fires off a Frostflake Arrow that deals <strong>Cryo DMG</strong><br>\
+                    &emsp; &emsp; The Frostflake Arrow blooms after hitting its target, dealing <strong>AoE Cryo DMG</strong><br><br>\
+                    Plunging Attack: Fires off a shower of arrows in mid-air before falling \
                     and striking the ground, dealing AoE DMG upon impact."
     },
     {
         name: "Trail of the Qilin", 
         description: "Leaving a single Ice Lotus behind, Ganyu dashes backward, shunning all impurity and \
-                    dealing AoE Cryo DMG.<br> <strong>Ice Lotus:</strong><br> 1. Continuously taunts surrounding\
+                    dealing AoE Cryo DMG.<br> Ice Lotus:<br> 1. Continuously taunts surrounding\
                     opponents, attracting them to attack it.<br> 2. Endurance scales based on Ganyu's Max HP.<br>\
-                    3. Blooms profusely when destroyed or once its duration ends, dealing AoE Cryo DMG."
+                    3. Blooms profusely when destroyed or once its duration ends, dealing <strong>AoE Cryo DMG.<strong>"
     },
     {
         name: "Celestial Shower",
         description: "Coalesces atmospheric frost and snow to summon a Sacred Cryo Pearl that exorcises evil.<br>\
                     During its ability duration, the Sacred Cryo Pearl will continuously rain down shards of ice, \
-                    striking opponents within an AoE and dealing Cryo DMG."
+                    striking opponents within an AoE and dealing <strong>Cryo DMG</strong>."
     },
     {
         name: "Undivided Heart",
         description: "After firing a Frostflake Arrow, the CRIT Rate of subsequent Frostflake Arrows and their \
-        resulting bloom effects is increased by 20% for 5s."
+        resulting bloom effects is increased by <strong>20%</strong> for 5s."
     },
     {
         name: "Harmony between Heaven and Earth",
-        description: "Celestial Shower grants a 20% Cryo DMG Bonus to active members in the AoE."
+        description: "Celestial Shower grants a 20% <strong>Cryo DMG Bonus</strong> to active members in the AoE."
     },
     {
         name: "Preserved for the Hunt",
-        description: "Refunds 15% of the ores used when crafting Bow-type weapons."
+        description: "Refunds <strong>15%</strong> of the ores used when crafting Bow-type weapons."
     }
 ]
 
