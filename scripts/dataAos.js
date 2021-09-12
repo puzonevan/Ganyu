6a35f44ae8881aeaa8f509857335715294354f3e
‘Evan <puzonevan@gmail.com>
2021-09-11 20:13:02 -0700

    weapon png fixed

--- scripts/dataAos.js
+++ scripts/dataAos.js
@@ -4,4 +4,4 @@ const animations = [
 
 document.getElementById("character-name").setAttribute("data-aos", "zoom-in");
 document.getElementById("character-name").setAttribute("data-aos-duration", "850");
-document.getElementById("character-name").setAttribute("data-aos-easing", "linear");
+// document.getElementById("character-name").setAttribute("data-aos-easing", "linear");
