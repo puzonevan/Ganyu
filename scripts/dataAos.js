724ea3a6cc465240ba514cb4b3b6ac6a1e0c0313
‘Evan <puzonevan@gmail.com>
2021-09-11 19:52:49 -0700

    js aos attribute sets

---
+++ scripts/dataAos.js
@@ -0,0 +1,7 @@
+const animations = [
+    
+]
+
+document.getElementById("character-name").setAttribute("data-aos", "zoom-out");
+document.getElementById("character-name").setAttribute("data-aos-duration", "850");
+document.getElementById("character-name").setAttribute("data-aos-easing", "linear");
