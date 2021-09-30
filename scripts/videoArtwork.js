9c51b64da6bbc94e962a669f5614a575e25999e6
‘Evan <puzonevan@gmail.com>
2021-09-29 19:14:56 -0700

    webp conversion on artwork

--- scripts/videoArtwork.js
+++ scripts/videoArtwork.js
@@ -5,15 +5,15 @@
  */
 
 const links = [
-    './ganyu/images/CloudsH.jpg',
-    './ganyu/images/Fan-1H.jpg',
-    './ganyu/images/Fan-2H.jpg',
-    './ganyu/images/Fan-3H.jpg',
-    './ganyu/images/GanyuLanterns.jpg',
-    './ganyu/images/GlazeLilliesH.png',
-    './ganyu/images/SmileH.png',
-    './ganyu/images/ganyustairs.png',
-    './ganyu/images/Fan-4H.png',
+    './ganyu/images/CloudsH.webp',
+    './ganyu/images/Fan-1H.webp',
+    './ganyu/images/Fan-2H.webp',
+    './ganyu/images/Fan-3H.webp',
+    './ganyu/images/GanyuLanterns.webp',
+    './ganyu/images/GlazeLilliesH.webp',
+    './ganyu/images/SmileH.webp',
+    './ganyu/images/ganyustairs.webp',
+    './ganyu/images/Fan-4H.webp',
 ]
 
 // Get the container for the art from DOM 
